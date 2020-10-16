6e9ea6c56ead852aeb6cd9530a0186108654b437
Ryan Rosiak <ryan11291129@gmail.com>
2020-10-16 18:30:28 -0400

    Added minor app changes

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, url_for
+import os
+import sys
+
 
 app = Flask(__name__)
 
 
+# Home
 @app.route('/')
-def index():
+def my_home():
     return render_template('index.html')
 
 
+# Redirect Pages
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+# App run
 if __name__ == "__main__":
     app.run(debug=True)
