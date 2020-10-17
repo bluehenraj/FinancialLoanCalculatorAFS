361eba2abb33f72d2c560eb55ce714e89bb2bc45
DevinParentice <dude114100@gmail.com>
2020-10-17 01:46:21 -0400

    Fixed duplicate forms

--- static/js/form.js
+++ static/js/form.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
 		currentSection.removeClass("is-active").next().addClass("is-active");
 		headerSection.removeClass("is-active").next().addClass("is-active");
 
-		if (currentSectionIndex === 15) {
+		if (currentSectionIndex === 9) {
 			$(document).find(".form-wrapper .section").first().addClass("is-active");
 			$(document).find(".steps li").first().addClass("is-active");
 		}
