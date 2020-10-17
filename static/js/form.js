e2dcae62add1f35c9be71a4cf742bd6bb668b5f1
DevinParentice <dude114100@gmail.com>
2020-10-16 23:50:52 -0400

    Fixing merge

--- static/js/form.js
+++ static/js/form.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 		currentSection.removeClass("is-active").next().addClass("is-active");
 		headerSection.removeClass("is-active").next().addClass("is-active");
 
-		if (currentSectionIndex === 3) {
+		if (currentSectionIndex === 6) {
 			$(document).find(".form-wrapper .section").first().addClass("is-active");
 			$(document).find(".steps li").first().addClass("is-active");
 		}
