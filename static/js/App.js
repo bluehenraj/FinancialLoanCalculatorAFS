02d5a571f66a2f564645394a62657d3412da46c1
DevinParentice <dude114100@gmail.com>
2020-10-17 08:49:39 -0400

    Started fixing bugs to quiz

--- static/js/App.js
+++ static/js/App.js
@@ -1,20 +1,2 @@
-import React from 'react';
-import logo from './logo.svg';
-import '../App.css';
-import BasicInfo from './BasicInfoSlide/BasicInfo';
-
-function App() {
-
-  console.log("here")
-  return (
-
-    <div className ="app"> 
-    fobar
-      <BasicInfo />
-    </div>
-  );
-}
-
-export default App;
 
 
