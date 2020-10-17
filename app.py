19a11aecf678aa8173c2b512905d951b52fd87db
Ryan Rosiak <ryan11291129@gmail.com>
2020-10-17 02:54:26 -0400

    Clean up

--- app.py
+++ app.py
@@ -27,39 +27,6 @@ def video_page():
 # Submitting form
 @app.route('/submitform', methods=['POST'])
 def form_submit():
-
-    """
-    # Mandatory
-    # Name
-    first_name = request.form['firstname']
-    last_name = request.form['lastname']
-    # Financial Information
-    income = request.form['incomeTotal']
-    debt_total = request.form['debtTotal']
-    # Can do without
-    # Housing Information
-    rent = request.form.get('rent')
-    prop_tax = request.form.get('propTaxes')
-    phone = request.form.get('phone')
-    # Utilities
-    power = request.form.get('power')
-    water = request.form.get('waterSewer')
-    garbage = request.form.get('garbagerecycling')
-    cable = request.form.get('bundlepackagecable')
-    # Healthcare
-    prescriptions = request.form.get('prescriptions')
-    doctor_visits = request.form.get('doctorvisits')
-    # Childcare
-    daycare = request.form.get('daycare')
-    # Automobile
-    carpayment1 = request.form.get('carpayment1')
-    carpayment2 = request.form.get('carpayment&2')
-    autoinsurance = request.form.get('autoinsurance')
-    gasoline = request.form.get('gasoline')
-    # Food
-    groceries = request.form.get('groceries')
-    pchi = request.form.get('personalcarehomeitems')
-    """
     if request.method == 'POST':
         try:
             data = request.form.to_dict()
