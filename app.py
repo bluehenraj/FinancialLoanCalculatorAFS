ae5a91c7bab243f2d9a953b6bf403f1828924d02
Jason <jason.rui.li7@gmail.com>
2020-10-16 22:55:17 -0400

    front end back end communicates now

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 import os
 import sys
 from dotenv import load_dotenv
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Load the environment variables
 load_dotenv()
 # Set the environment variable
-database_url = os.environ["DATABASE_URL"]
+database_url = os.environ["DATABASE_URL"] 
 
 # Home
 @app.route('/')
@@ -19,6 +19,44 @@ def my_home():
 @app.route('/form')
 def form_page():
     return render_template('form.html')
+
+@app.route('/quiz')
+def quiz_page():
+    return render_template('quiz.html')
+
+# Submitting form
+@app.route('/submitform', methods=['POST'])
+def form_submit():
+
+    first_name = request.form['firstname']
+    last_name = request.form['lastname']
+
+    income = request.form['incomeTotal']
+    debtTotal = request.form['debtTotal']
+
+    rent = request.form.get('rent')
+    propTax = request.form.get('propTaxes')
+    phone = request.form.get('phone')
+
+    power = request.form.get('power')
+    waterSewer = request.form.get('waterSewer')
+    insurance = request.form.get('insurance')
+
+    print(first_name)
+    print(last_name)
+
+    print(income)
+    print(debtTotal)
+
+    print(rent)
+    print(propTax)
+    print(phone)
+
+    print(power)
+    print(waterSewer)
+    print(insurance)
+
+    return  render_template('index.html')
 
 # App run
 if __name__ == "__main__":
