9c7e87eba9163c8dca992c1b837e41b55af86429
DevinParentice <dude114100@gmail.com>
2020-10-17 10:13:54 -0400

    Fixed form validation

--- static/js/form.js
+++ static/js/form.js
@@ -9,7 +9,6 @@ $(document).ready(function () {
 		currentSection.removeClass("is-active").prev().addClass("is-active");
 		headerSection.removeClass("is-active").prev().addClass("is-active");
 		currentSectionIndex -= 1;
-		checkSection(currentSectionIndex);
 	});
 
 	$(".form-wrapper div.button").click(function () {
@@ -21,58 +20,54 @@ $(document).ready(function () {
 		var lastName = document.getElementById("lastname");
 		var incomeTotal = document.getElementById("incomeTotal");
 		var debtTotal = document.getElementById("debtTotal");
+		if (currentSectionIndex === 0) {
+			if (firstName && !firstName.value) {
+				alert("Please tell us your first name!");
+				return false;
+			} else if (lastName && !lastName.value) {
+				alert("Please tell us your last name!");
+				return false;
+			}
+			document.getElementById("tips").innerHTML = "Filling out the information as accurately as possible can give you the best quote!";
+		}
+		else if (currentSectionIndex === 1) {
+			if (incomeTotal && !incomeTotal.value) {
+				alert("Please tell us your income!");
+				return false;
+			} else if (debtTotal && !debtTotal.value) {
+				alert("Please tell us your debt amount!");
+				return false;
+			}
+			document.getElementById("tips").innerHTML = "2 in 10 Americans spend at least 50% of their monthly income on debt repayment.";
+		}
+		else if (currentSectionIndex === 2) {
+			document.getElementById("tips").innerHTML = "21% of households do not read their bills carefully.";
+		}
+		else if (currentSectionIndex === 3) {
+			document.getElementById("tips").innerHTML = "United States citizens spent a total of 3.6 trillion in 2019 on their healthcare.";
+		}
+		else if (currentSectionIndex === 4) {
+			document.getElementById("tips").innerHTML = "The rise in food prices has pushed 44 million people into poverty since 2010.";
+		}
+		else if (currentSectionIndex === 5) {
+			document.getElementById("tips").innerHTML = "The average American's car payment is a whopping $545 per month.";
+		}
+		else if (currentSectionIndex === 6) {
+			document.getElementById("tips").innerHTML = "You're almost done!";
+		}
+		else if (currentSectionIndex === 7) {
+			document.getElementById("tips").innerHTML = "American Financial Solutions has plenty of programs to help!";
+		}
 		currentSection.removeClass("is-active").next().addClass("is-active");
 		headerSection.removeClass("is-active").next().addClass("is-active");
-		checkSection(currentSectionIndex, firstName, lastName, incomeTotal, debtTotal);
+
+		if (currentSectionIndex === 9) {
+			$(document).find(".form-wrapper .section").first().addClass("is-active");
+			$(document).find(".steps li").first().addClass("is-active");
+		}
 		bounce($('.tipman'));
 	});
 });
-
-function checkSection(currentSectionIndex, firstName, lastName, incomeTotal, debtTotal) {
-	if (currentSectionIndex === 0) {
-		if (firstName && !firstName.value) {
-			alert("Please tell us your first name!");
-			return false;
-		} else if (lastName && !lastName.value) {
-			alert("Please tell us your last name!");
-			return false;
-		}
-		document.getElementById("tips").innerHTML = "Filling out the information as accurately as possible can give you the best quote!";
-	}
-	else if (currentSectionIndex === 1) {
-		if (incomeTotal && !incomeTotal.value) {
-			alert("Please tell us your income!");
-			return false;
-		} else if (debtTotal && !debtTotal.value) {
-			alert("Please tell us your debt amount!");
-			return false;
-		}
-		document.getElementById("tips").innerHTML = "2 in 10 Americans spend at least 50% of their monthly income on debt repayment.";
-	}
-	else if (currentSectionIndex === 2) {
-		document.getElementById("tips").innerHTML = "21% of households do not read their bills carefully.";
-	}
-	else if (currentSectionIndex === 3) {
-		document.getElementById("tips").innerHTML = "United States citizens spent a total of 3.6 trillion in 2019 on their healthcare.";
-	}
-	else if (currentSectionIndex === 4) {
-		document.getElementById("tips").innerHTML = "The rise in food prices has pushed 44 million people into poverty since 2010.";
-	}
-	else if (currentSectionIndex === 5) {
-		document.getElementById("tips").innerHTML = "The average American's car payment is a whopping $545 per month.";
-	}
-	else if (currentSectionIndex === 6) {
-		document.getElementById("tips").innerHTML = "You're almost done!";
-	}
-	else if (currentSectionIndex === 7) {
-		document.getElementById("tips").innerHTML = "American Financial Solutions has plenty of programs to help!";
-	}
-	if (currentSectionIndex === 9) {
-		$(document).find(".form-wrapper .section").first().addClass("is-active");
-		$(document).find(".steps li").first().addClass("is-active");
-	}
-}
-
 function bounce(thing) {
 	var interval = 100;
 	var distance = 20;
